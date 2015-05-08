04b6920dd378a2372c352060ce5226cd9ac69802
Jon Mueller <jamuell2@illinois.edu>
2015-05-08 13:04:52 -0500

    added basic server, only works with http google pages not https

--- server/server.py
+++ server/server.py
@@ -2,7 +2,6 @@ import tornado.escape
 import tornado.ioloop
 import tornado.web
 import webbrowser
-import json_parse
 import json
 import os
 import time
@@ -13,18 +12,17 @@ PORT = 8888
 #defines what html file to open. Says to open the bar time series
 class URLHandler(tornado.web.RequestHandler):
     def post(self):
-        urls = self.get_argument('url')
+        urls = self.get_argument('sites')
         print urls
         self.write({"test": 1});
 
 #says what file to open based on the url paths
 application = tornado.web.Application(handlers=[
-    (r"/", URLHandler)],
+    (r"/urls", URLHandler)],
     static_path=os.path.join(os.path.dirname(__file__), '.'),
     debug=True
 )
 
 if __name__ == "__main__":
     application.listen(PORT)
-    tornado.ioloop.IOLoop.instance().add_callback(callback=lambda:webbrowser.open_new('http://localhost:' + str(PORT) + '/'))
     tornado.ioloop.IOLoop.instance().start()
