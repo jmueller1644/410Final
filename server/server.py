cb269e2c6657da8fbfecc939b656071101aa2fea
Jon Mueller <jamuell2@illinois.edu>
2015-05-09 16:56:43 -0500

    removed scripts from html

--- server/server.py
+++ server/server.py
@@ -11,9 +11,8 @@ class URLHandler(tornado.web.RequestHandler):
     def post(self):
         urls = filter(lambda u: str(u) != 'None', [str(url) for url in json.loads(self.get_argument('sites'))])
         contents = [requests.get(url).content for url in urls]
-        pyq_docs = [pq(content) for content in contents]
-        inner_texts = [doc.text() for doc in pyq_docs]
-
+        pyq_docs = [pq(content).remove("script").remove("style") for content in contents]
+        inner_texts = [doc("body").text() for doc in pyq_docs]
         self.write({"linkTexts": inner_texts})
 
 #says what file to open based on the url paths
