0b674d60a0bb3fc2fbf6ad93c93b98650e8ddc3c
Jon Mueller <jamuell2@illinois.edu>
2015-05-13 21:10:52 -0500

    added server changes

--- server/server.py
+++ server/server.py
@@ -13,26 +13,15 @@ model_file_name='tfidf_perceptron_model.p'
 class TrainHandler(tornado.web.RequestHandler):
     def get(self):
         get_dict = self.request.arguments
-        category = get_dict['category'][0]
-        print category
-        # urls = filter(lambda u: str(u) != 'None', [str(url) for url in json.loads(get_dict['site'][0])])
-        # inner_texts = []
-        # for url in urls:
-        #     print url
-        #     inner_texts.append(get_url_text(url))
-        # pyq_docs = [pq(content).remove("script").remove("style") for content in contents]
-        # inner_texts = [doc("body").text() for doc in pyq_docs]
-
+        category = int(get_dict['category'][0])
         model.train([get_url_text(get_dict['site'][0])],[category]);
-        #print predictions
         self.write({"results": "done"})
 
 class SaveHandler(tornado.web.RequestHandler):
     def get(self):
         with open(model_file_name, 'wb') as f:
-            model = pickle.dump(model, f)
+            pickle.dump(model, f)
         
-        #print predictions
         self.write({"results": "done"})
 
 class URLHandler(tornado.web.RequestHandler):
@@ -42,12 +31,8 @@ class URLHandler(tornado.web.RequestHandler):
         for url in urls:
             print url
             inner_texts.append(get_url_text(url))
-        # pyq_docs = [pq(content).remove("script").remove("style") for content in contents]
-        # inner_texts = [doc("body").text() for doc in pyq_docs]
-
         
         predictions = [model.predict(text) for text in inner_texts]
-        #print predictions
         self.write({"results": predictions})
 
 #says what file to open based on the url paths
